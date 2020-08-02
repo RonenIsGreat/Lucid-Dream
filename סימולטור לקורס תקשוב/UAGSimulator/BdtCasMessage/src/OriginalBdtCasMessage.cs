3b4b1586fd7c8a106eaca0d62174db50bb34fc35
RonenIsGreat <96ronen@gmail.com>
2020-08-02 13:32:35 -0700

    Added simulator project

---
+++ סימולטור לקורס תקשוב/UAGSimulator/BdtCasMessage/src/OriginalBdtCasMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GlobalResources.BasicData;
+
+
+namespace BdtCasMessage
+{
+   public class OriginalBdtCasMessage
+    {
+        public TimeType timeStamp;
+        public List<TrackData> systemTracks;
+    }
+
+    public struct TrackData
+    {
+        public long trackID;
+        public State trackState;
+        public TimeType creationTime;
+        public float relativeBearing;
+        public float relativeBearingRate;
+    }
+    public enum State
+    {
+        NewTrack,
+        UpdateTrack,
+        DeleteTrack
+    }
+
+}
---
+++ סימולטור לקורס תקשוב/UAGSimulator/BeamBusCas/BeamBusCasSender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace BeamBusCas
+{
+    public static class BeamBusCasSender
+    {
+        static int subSegmentNum;
+        static byte[][] subSements;
+        static UDPSocket client;
+
+        public static void SendMessage()
+        {
+            subSements = FileEdit.GetRecording(Properties.Settings.Default.Recording_path);
+            client = new UDPSocket();
+            client.Client(Properties.Settings.Default.IP, 
+                Properties.Settings.Default.Port);
+            subSegmentNum = subSements.Length;
+
+            while (true)
+            {
+                for (int j = 0; j < subSegmentNum; j++)
+                {
+                    client.Send(subSements[j]);
+                    delayInMs(0.1);
+                }
+
+            }
+        }
+
+        public static void SendMessageOnce()
+        {
+            subSements = FileEdit.GetRecording(Properties.Settings.Default.Recording_path);
+            client = new UDPSocket();
+            client.Client(Properties.Settings.Default.IP,
+                Properties.Settings.Default.Port);
+            subSegmentNum = subSements.Length;
+
+            for (int j = 0; j < subSegmentNum; j++)
+            {
+                client.Send(subSements[j]);
+                delayInMs(0.1);
+            }
+        }
+
+        private static void delayInMs(double ms)
+        {
+            for (int i = 0; i < ms * 280000; i++)
+            {
+
+            }
+        }
+    }
+}
+
+
